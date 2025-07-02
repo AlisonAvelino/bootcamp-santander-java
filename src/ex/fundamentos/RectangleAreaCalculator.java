9fd045c7195fb626f2fedd2114d6338de10eb064
Alison Avelino <alisonavelino3133@gmail.com>
2025-07-02 14:26:45 -0300

    "[TASK_004]-feat: Exercicio 03 de fundamentos"

---
+++ src/ex/fundamentos/RectangleAreaCalculator.java
@@ -0,0 +1,24 @@
+package ex.fundamentos;
+
+import java.util.Scanner;
+
+public class RectangleAreaCalculator {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+
+        System.out.println("Informe o tamanho da base do retângulo:");
+        double base = scanner.nextDouble();
+
+        System.out.println("Informe o tamanho da altura do retângulo:");
+        double height = scanner.nextDouble();
+
+        double area = calculateRectangleArea(base, height);
+        System.out.printf("A área do retângulo é %.2f\n", area);
+    }
+
+    private static double calculateRectangleArea(double base, double height) {
+        double area = base * height;
+        return area;
+    }
+}
+
